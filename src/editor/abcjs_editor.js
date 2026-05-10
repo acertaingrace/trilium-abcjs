7e841256bcf921f91375c0311598ee7ee75e0361
acertaingrace <dorothy.duyen@gmail.com>
2026-05-10 11:53:54 +1000

    Integrates highlightjs into editor. Tentatively closes #1

--- src/editor/abcjs_editor.js
+++ src/editor/abcjs_editor.js
@@ -1,17 +1,39 @@
-// import library
+// import libraries
 const abcjs = require('abcjs_basic.js');
+const hljs = require('highlight.min.js');
+const hljsAbc = require('hljs_abc');
 
 // global variables
 const saveButton = document.getElementById("saveButton");
-const textArea = document.getElementById("abc");
+const textArea = document.getElementById("abc");			// highlightjs div
+const invText = document.getElementById("invisibleText");	// textarea
 const saveFile = await getAbcSave();
 
 // main statements
 await textareaFromFile();
 saveButton.addEventListener("click", clickSave);
 
+// to loophole over contenteditable div vs textarea-abcjs editor
+textArea.innerHTML = invText.value;
+invText.addEventListener("input", function(){
+    textArea.innerHTML = invText.value;
+});
+invText.addEventListener("scroll", function(){
+    $('#abc').scrollTop($('#invisibleText').scrollTop());
+});
+
+/* attempting to select contenteditable div text directly
+preserved for posterity
+textArea.addEventListener("selectstart", function(){
+    await textareaFromFile();
+    var selection = document.getSelection();
+    console.log(selection);
+    invText.setSelectionRange(selection.anchorOffset, selection.focusOffset);
+    console.log("Selecting " + selection.anchorOffset + " to " + selection.focusOffset);
+});*/
+
 // use abcjs's Editor
-const editor = new abcjs.Editor("abc", { 
+const editor = new abcjs.Editor("invisibleText", {
     canvas_id: "paper", 
     warnings_id:"warnings",
     abcjsParams: {
@@ -21,12 +43,24 @@ const editor = new abcjs.Editor("abc", {
 });
 editor.fireChanged();
 
+// use highlight.js & hljs-abc
+hljs.registerLanguage("abc", hljsAbc.main);
+hljs.highlightAll();
+
+// redoing highlighting when content is edited
+invText.addEventListener("input", function(){
+    console.log("Text edited - highlighting all...");
+    textArea.removeAttribute('data-highlighted');
+    hljs.highlightAll();
+});
+
 /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 
 Event handler for clicking save button
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
 async function clickSave(event){
     // set file content
-    await api.runOnBackend((noteId, content) => api.getNote(noteId).setContent(content), [saveFile.noteId, textArea.value]);
+    await api.runOnBackend((noteId, content) => api.getNote(noteId).setContent(content), [saveFile.noteId, invText.value]);
+    //await api.runOnBackend((noteId, content) => api.getNote(noteId).setContent(content), [saveFile.noteId, textArea.innerText]);
 }
 
 /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -38,7 +72,9 @@ async function textareaFromFile(){
     const saveData = await api.runOnBackend((id) => api.getNote(id).getContent(), [saveFile.noteId]);
 
     // display content in textarea
-    textArea.value = saveData;
+    invText.value = saveData;
+    //textArea.textContent = saveData;
+    //textArea.value = "X:1\nK:D\nDD AA|BBA2|\n";
 }
 
 /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
---
+++ src/editor/hljs_abc.js
@@ -0,0 +1,94 @@
+/*
+  Language: Abc
+  Description: Abc Music Notation Language
+  Author: NriotHrreion <nriot233@gmail.com>
+  Website: https://abcnotation.com
+  Category: common
+*/
+
+/* Altered to use module.exports instead of export default */
+
+function main(hljs) {
+  const accidentalDirectivePrefix = `(?<=%%(propagate|writeout)-accidentals.*)`;
+  const fontDirectivePrefix = `(?<=%%(title|subtitle|composer|parts|tempo|gchord|annotation|info|text|vocal|words|set)font.*)`;
+  const textDirectivePrefix = `(?<=%%(text|center)\\s)`;
+
+  return {
+    name: "Abc Notation",
+    aliases: ["abc"],
+    contains: [
+      // fields
+      {
+        scope: "meta",
+        match: /^[XTCOAMLQPZNGHKRBDFSImrsUVWw+]:/
+      },
+      // text content in fields
+      {
+        scope: "meta string",
+        match: /(?<=[COAZHBDFS]:).*/
+      },
+      // title content, lyrics content
+      {
+        scope: "title",
+        match: /(?<=[TWw]:).*/
+      },
+      // duplets, triplets, quadruplets, etc.
+      {
+        scope: "operator",
+        match: /\([2-9]/
+      },
+      // chord symbols
+      hljs.QUOTE_STRING_MODE,
+      // numbers
+      hljs.NUMBER_MODE,
+      // decorations
+      {
+        scope: "string",
+        begin: "!", end: "!"
+      },
+      // repeat symbols, bar symbols
+      {
+        scope: "operator",
+        match: /(\|\||::|\[?:*\|(:|\[?[1-9])*\]?)/
+      },
+      {
+        scope: "operator",
+        match: /\[".+?"/
+      },
+      // ties, slurs, grace notes, chords, unisons
+      {
+        scope: "operator",
+        match: /(?<!(\w:|%%).*)(-|\(|\)|{|}|\[|\])/
+      },
+      // macro expressions
+      {
+        scope: "operator",
+        match: "="
+      },
+      // directives
+      {
+        scope: "keyword",
+        match: /^%%\S+/
+      },
+      // accidental directives
+      {
+        scope: "keyword",
+        match: new RegExp(accidentalDirectivePrefix +"(not|octave|pitch|none|added|all)")
+      },
+      // font directives
+      {
+        scope: "literal",
+        match: new RegExp(fontDirectivePrefix +"\\*")
+      },
+      // text directives
+      {
+        scope: "string",
+        match: new RegExp(textDirectivePrefix +".*")
+      },
+      // comments
+      hljs.COMMENT("%", "$"),
+    ]
+  };
+}
+
+module.exports.main = main
